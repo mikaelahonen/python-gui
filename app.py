79f5501bf2a53c4248ef4ad2156e5e61c759a915
Mikael Ahonen <mikael.ahonen@hotmail.com>
2019-08-25 20:27:35 +0300

    Initial commit.

---
+++ app.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+frame_width = 800
+frame_height = 450
+
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)               
+        self.master = master
+        self.init_window()
+        
+    def init_window(self):
+        
+        #Change the title of window      
+        self.master.title("My Python GUI")
+
+        #Fill the window
+        self.pack(fill=BOTH, expand=1)
+
+        #Create a button
+        action_button = Button(self, text="Print")        
+        action_button.place(x=0, y=0)
+        #action_button.size(height=100, width=100)
+
+        
+        
+#Initialize Tkinter
+root = Tk()
+
+#size of the window
+root_geometry = "{}x{}".format(frame_width, frame_height)
+root.geometry(root_geometry)
+
+#Get the window class, pass the root variable
+app = Window(root)
+
+#The program constantly checks for updates
+root.mainloop()
